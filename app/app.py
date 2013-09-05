d113c6c5e444f5c7340152f7f8ab7bb6e7e5932d
Calum McHaffie <calum_mch@hotmail.com>
2013-09-05 14:30:28 +1200

    guru

--- app/app.py
+++ app/app.py
@@ -1,1 +1,18 @@
-println "Hello World again"
+
+class Guru():
+    def __init__(self, name=""):
+        self.name = name
+        self.user = ''
+        
+    def greeting(self, user=""):
+        self.user = user
+        return "Hello "+user+", I am "+self.name+" the Guru"
+            
+    def ask(self, question):
+        if (self.user == ''):
+            return "You lazy fool, greet me before asking such a question"
+        elif (self.user == 'Shakeel'):
+            return "You cannot afford my wisdom"
+        else:
+            return "That is a good question, let me think about it"
+
